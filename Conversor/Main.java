7e43a2e414adcf3b55b1342c4e42eb191ef8ed24
Falik <102031226+SrFalik@users.noreply.github.com>
2022-05-28 13:49:15 -0300

    Add files via upload

--- Conversor/Main.java
+++ Conversor/Main.java
@@ -25,9 +25,24 @@ public class Main {
                         }
                         info = novo;
                     }
-                    list.add(new Paciente(info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9],info[10], info[11], info[12], info[13], info[14], info[15], info[16], info[17], info[18], info[19],
-                            info[20], info[21], info[22], info[23], info[24], info[25], info[26], info[27], info[28], info[29], info[30], info[31], info[32], info[33], info[34], info[35], info[36], info[37], info[38], info[39],
-                            info[40], info[41], info[42], info[43], info[44], info[45], info[46], info[47], info[48], info[49], info[50], info[51]));
+
+                    if(!info[0].equalsIgnoreCase("CODIGO")) {
+                        list.add(new Paciente(
+                                info[0],
+                                info[1],
+                                info[11],
+                                info[12],
+                                info[43],
+                                info[45],
+                                info[47],
+                                info[48],
+                                info[49],
+                                info[24],
+                                info[25],
+                                info[26],
+                                info[16],
+                                info[17]));
+                    }
                 });
 
                 for (Paciente p : list) {
@@ -36,13 +51,11 @@ public class Main {
                     System.out.println(p);
                 }
 
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            } catch (IOException | ArrayIndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
             }
 
-            String pathOut = "C:\\Users\\Falik\\Documents\\connazinica\\PacienteCebola.txt";
+        String pathOut = "C:\\Users\\Falik\\Documents\\connazinica\\PacienteCebola.txt";
         try (BufferedWriter bw = new
                 BufferedWriter(new FileWriter(pathOut))) {
             for (Paciente p : list) {
@@ -50,7 +63,7 @@ public class Main {
                 bw.newLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 }
